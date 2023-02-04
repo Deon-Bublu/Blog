f30836877b9632140a745b44d3b3c0a06e426b78
Deon-Bublu <Osetrov01v1@list.ru>
2023-02-04 12:48:04 +0500

    теперь отображается отдельная статья

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 	
 	
 	get "/articles", to: "articles#index"
+	get "/articles/:id", to: "atricles#show"
+	
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
